52785ce72e9f250df081ea1442229446a02c716a
Alex Price <hi@alexpriceonline.com>
2019-11-22 17:15:32 +0000

    Removed lerna log and ui dist

--- packages/open-ui/dist/components/Button/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Button_1 = require("./Button");
-exports.Button = Button_1.Button;
--- packages/open-ui/dist/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Button_1 = require("./components/Button");
-exports.Button = Button_1.Button;
