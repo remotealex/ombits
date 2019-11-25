308b7879c4054cdfd68e1bd3d3723b3eadf03e5b
Alex Price <hi@alexpriceonline.com>
2019-11-25 09:21:39 +0000

    Added rollup to open-ui to bundle the package for external use

--- packages/open-ui/src/main.tsx
+++ packages/open-ui/src/main.tsx
@@ -1,3 +1,8 @@
-import { Button } from './components/Button';
-
-export { Button };
+export * from './components/AutoGrid';
+export * from './components/Button';
+export * from './components/Card';
+export * from './components/Grid';
+export * from './components/Navigation';
+export * from './components/Outline';
+export * from './components/Typography';
+export * from './components/Wrapper';
