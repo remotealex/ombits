c95a52b3985305f618b4aa65a003fb2490e0c9dc
gurjit03 <gurjitmehta@gmail.com>
2019-11-27 03:30:51 +0530

    renamed Tag component prop from name to text

--- packages/open-ui/src/components/Tag/Tag.tsx
+++ packages/open-ui/src/components/Tag/Tag.tsx
@@ -7,20 +7,20 @@ import { getMarginProps } from 'utils/helpers';
 import styles from './Tag.module.scss';
 
 interface TagProps {
-  name: string;
-  size?: 'small' | 'large';
+  text: string;
+  size?: 'large';
 }
 
 export const Tag: React.FC<TagProps & MarginStyleKeys> = props => {
-  const { name, size = 'small' } = props;
+  const { text, size } = props;
   const _props = { marginBottom: 0.5, marginRight: 0.5, ...props };
   const marginStyles = getMarginProps(_props);
-  const cls = classNames(styles.Tag, {
+  const cls = classNames(styles.tag, {
     [styles.large]: size === 'large',
   });
   return (
     <div style={{ ...marginStyles }} className={cls}>
-      {name}
+      {text}
     </div>
   );
 };
