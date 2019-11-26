f3552bbb2ca6f09376bf3eddea6030a606c3a303
gurjit03 <gurjitmehta@gmail.com>
2019-11-27 03:38:09 +0530

    updated the Tag.stories to include text prop inplace of name

--- packages/open-ui/src/components/Tag/Tag.stories.tsx
+++ packages/open-ui/src/components/Tag/Tag.stories.tsx
@@ -4,17 +4,17 @@ import { Tag } from './Tag';
 
 export const standard = () => (
   <React.Fragment>
-    <Tag name="tag1" />
-    <Tag name="tag2" />
-    <Tag name="tag3" />
+    <Tag text="tag1" />
+    <Tag text="tag2" />
+    <Tag text="tag3" />
   </React.Fragment>
 );
 
 export const largeSize = () => (
   <React.Fragment>
-    <Tag size="large" name="tag1" />
-    <Tag size="large" name="tag2" />
-    <Tag size="large" name="tag3" />
+    <Tag size="large" text="tag1" />
+    <Tag size="large" text="tag2" />
+    <Tag size="large" text="tag3" />
   </React.Fragment>
 );
 
