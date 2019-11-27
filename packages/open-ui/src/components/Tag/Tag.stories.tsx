d31a53ff0cde6d5a3e94ff140dc9a383be89672a
Alex Price <hi@alexpriceonline.com>
2019-11-27 20:39:55 +0000

    Updated tag colours

--- packages/open-ui/src/components/Tag/Tag.stories.tsx
+++ packages/open-ui/src/components/Tag/Tag.stories.tsx
@@ -2,19 +2,53 @@ import React from 'react';
 
 import { Tag } from './Tag';
 
+const common = { text: 'Taggy', marginRight: 1 };
+
 export const standard = () => (
   <React.Fragment>
-    <Tag text="tag1" />
-    <Tag text="tag2" />
-    <Tag text="tag3" />
+    <Tag {...common} />
+    <Tag {...common} />
+    <Tag {...common} />
   </React.Fragment>
 );
 
-export const largeSize = () => (
+export const secondary = () => (
   <React.Fragment>
-    <Tag size="large" text="tag1" />
-    <Tag size="large" text="tag2" />
-    <Tag size="large" text="tag3" />
+    <Tag {...common} intent="secondary" />
+    <Tag {...common} intent="secondary" />
+    <Tag {...common} intent="secondary" />
+  </React.Fragment>
+);
+
+export const success = () => (
+  <React.Fragment>
+    <Tag {...common} intent="success" />
+    <Tag {...common} intent="success" />
+    <Tag {...common} intent="success" />
+  </React.Fragment>
+);
+
+export const error = () => (
+  <React.Fragment>
+    <Tag {...common} intent="error" />
+    <Tag {...common} intent="error" />
+    <Tag {...common} intent="error" />
+  </React.Fragment>
+);
+
+export const warning = () => (
+  <React.Fragment>
+    <Tag {...common} intent="warning" />
+    <Tag {...common} intent="warning" />
+    <Tag {...common} intent="warning" />
+  </React.Fragment>
+);
+
+export const large = () => (
+  <React.Fragment>
+    <Tag {...common} size="large" />
+    <Tag {...common} size="large" />
+    <Tag {...common} size="large" />
   </React.Fragment>
 );
 
--- packages/open-ui/src/components/Tag/Tag.tsx
+++ packages/open-ui/src/components/Tag/Tag.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import classNames from 'classnames';
 
 import { MarginStyleKeys } from 'interfaces/MarginStyleKeys';
+import { IntentOption } from 'interfaces/IntentOption';
 import { getMarginProps } from 'utils/helpers';
-
+import { toSentenceCase } from 'utils/strings/to-sentence-case';
 import styles from './Tag.module.scss';
 
 interface TagProps {
-  text: string;
+  intent?: IntentOption;
   size?: 'large';
+  text: string;
 }
 
 export const Tag: React.FC<TagProps & MarginStyleKeys> = props => {
-  const { text, size, ...marginStyles } = props;
-  const _marginStyles = getMarginProps(marginStyles);
+  const { intent, size, text } = props;
+  const marginStyles = getMarginProps(props);
+
   const cls = classNames(styles.tag, {
     [styles.large]: size === 'large',
+    [styles[`intent${toSentenceCase(intent)}`]]: intent,
   });
+
   return (
-    <div style={{ ..._marginStyles }} className={cls}>
+    <div style={marginStyles} className={cls}>
       {text}
     </div>
   );
