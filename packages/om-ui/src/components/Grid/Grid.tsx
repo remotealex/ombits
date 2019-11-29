274061ea2b2688af961800a6f81f98cfc37191de
Alex Price <hi@alexpriceonline.com>
2019-11-29 20:43:54 +0000

    Gutter change

--- packages/om-ui/src/components/Grid/Grid.tsx
+++ packages/om-ui/src/components/Grid/Grid.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { MarginStyleKeys } from 'interfaces/MarginStyleKeys';
+import { getMarginProps } from 'utils/helpers';
 import styles from './Grid.module.scss';
 
 interface GridProps {
   gutters?: boolean;
 }
 
-export const Grid: React.FC<GridProps> = props => {
+export const Grid: React.FC<GridProps & MarginStyleKeys> = props => {
   const { children, gutters } = props;
+  const marginProps = getMarginProps(props);
 
   const cls = classNames(styles.grid, {
     [styles.gutters]: gutters,
   });
 
-  return <div className={cls}>{children}</div>;
+  return (
+    <section className={cls} style={marginProps}>
+      {children}
+    </section>
+  );
 };
